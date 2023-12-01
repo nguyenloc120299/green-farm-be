fd0faf8dbc9258223f2605584df6b0c7fc917001
TruongNguyen <141603366+truongnguyenhopper@users.noreply.github.com>
2023-12-01 16:01:07 +0700

    buy land

---
+++ src/database/repository/MyLandRepo.ts
@@ -0,0 +1,33 @@
+import MyLand, { MyLandModel } from "database/model/MyLand";
+import { Types } from "mongoose";
+
+async function findById(id: Types.ObjectId): Promise<MyLand | null> {
+  return MyLandModel.findOne({ _id: id, status: true }).lean().exec();
+}
+
+async function create(land: MyLand): Promise<MyLand> {
+  const created = await MyLandModel.create(land);
+  return created.toObject();
+}
+
+async function update(land: MyLand): Promise<MyLand | null> {
+  return MyLandModel.findByIdAndUpdate(land._id, land, { new: true })
+    .lean()
+    .exec();
+}
+
+async function findByLandId(land_id: number,user_id:Types.ObjectId): Promise<MyLand | null> {
+  return MyLandModel.findOne({
+    land_id,
+    user_id
+  })
+    .lean()
+    .exec();
+}
+
+export default {
+  findById,
+  create,
+  update,
+  findByLandId
+};
---
+++ src/routes/client/land/index.ts
@@ -0,0 +1,39 @@
+import { ProtectedRequest } from "app-request";
+import authentication from "auth/authentication";
+import { lands } from "config";
+import { BadRequestError } from "core/ApiError";
+import { BadRequestResponse, SuccessResponse } from "core/ApiResponse";
+import MyLand, { Category } from "database/model/MyLand";
+import MyLandRepo from "database/repository/MyLandRepo";
+import UserRepo from "database/repository/UserRepo";
+import express from "express";
+import { LAND } from "game-type";
+import asyncHandler from "helpers/asyncHandler";
+import _ from "lodash";
+
+const router = express.Router();
+
+/*-------------------------------------------------------------------------*/
+router.use(authentication);
+/*-------------------------------------------------------------------------*/
+
+router.post(
+  "/buy-land",
+  asyncHandler(async (req: ProtectedRequest, res) => {
+    const { land_id } = req.body;
+    const user = await UserRepo.findPrivateProfileById(req.user._id);
+    if (!user) throw new BadRequestError("User not registered");
+    const land = lands.find((i) => i.id === land_id);
+    if (!land) return new BadRequestResponse("Land not found").send(res);
+    if (user.money_balance || 0 < land.price)
+      return new BadRequestResponse("You don't have enough money").send(res);
+    const myLand = await MyLandRepo.findByLandId(land_id, user._id);
+    if (myLand) return new BadRequestResponse("You bought this land").send(res);
+    const newLand = await MyLandRepo.create({
+      category: Category.NO_PLANT,
+      land_id: land_id,
+      user_id: user._id,
+    } as MyLand);
+    return new SuccessResponse("Buy success", newLand).send(res);
+  })
+);
