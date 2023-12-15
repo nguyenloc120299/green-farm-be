3bdec1399ec4767be2fff77cbfa05177fc5ec4ab
TruongNguyen <141603366+truongnguyenhopper@users.noreply.github.com>
2023-12-15 16:07:03 +0700

    update

--- src/routes/client/land/index.ts
+++ src/routes/client/land/index.ts
@@ -1,8 +1,11 @@
 import { ProtectedRequest } from "app-request";
 import authentication from "../../../auth/authentication";
-import { PRICE_LAND_BUY, lands } from "../../../config";
+import { PRICE_LAND_BUY, lands, plants } from "../../../config";
 import { BadRequestError } from "../../../core/ApiError";
-import { BadRequestResponse, SuccessResponse } from "../../../core/ApiResponse";
+import {
+  BadRequestResponse,
+  SuccessResponse,
+} from "../../../core/ApiResponse";
 import MyLand, { Category } from "../../../database/model/MyLand";
 import MyLandRepo from "../../../database/repository/MyLandRepo";
 import UserRepo from "../../../database/repository/UserRepo";
@@ -36,9 +39,42 @@ router.post(
       land_id: land_id,
       user_id: user._id,
     } as MyLand);
-    user.money_balance= user.money_balance - PRICE_LAND_BUY
-    await UserRepo.updateInfo(user)
+    user.money_balance = user.money_balance - PRICE_LAND_BUY;
+    await UserRepo.updateInfo(user);
     return new SuccessResponse("Buy success", newLand).send(res);
   })
 );
-export default router
+
+router.post(
+  "/plant",
+  asyncHandler(async (req: ProtectedRequest, res) => {
+    const { land_id, plant_id } = req.body;
+    const user = await UserRepo.findPrivateProfileById(req.user._id);
+    if (!user) throw new BadRequestError("User not registered");
+    const myLand = await MyLandRepo.findByLandId(land_id, user._id);
+    if (!myLand)
+      return new BadRequestResponse(
+        "You have not purchased this plot of land yet"
+      ).send(res);
+    if (myLand.category != Category.NO_PLANT)
+      return new BadRequestResponse(
+        "This plot of land already has plants"
+      ).send(res);
+    const plantCurrent = plants.find((p) => p.id == plant_id);
+    if (!plantCurrent)
+      return new BadRequestResponse("This plant was not found").send(res);
+    if (!user.money_balance || user.money_balance < plantCurrent.price)
+      return new BadRequestResponse("You don't have enough money").send(res);
+    myLand.status = true;
+    myLand.plant_id = plant_id;
+    myLand.category=Category.PLANTING
+    myLand.time_start = new Date().getTime();
+    myLand.time_end = new Date().getTime() + plantCurrent.time_harvest;
+    myLand.harvest_balance = plantCurrent.harvest_balance;
+    await MyLandRepo.update(myLand);
+    user.money_balance = user.money_balance - PRICE_LAND_BUY;
+    await UserRepo.updateInfo(user);
+    return new SuccessResponse("Planted success", myLand).send(res);
+  })
+);
+export default router;
